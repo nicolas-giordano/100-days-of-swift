d5d0db701de2df1055095356a7a15f852d5204c9
Nicolas Giordano <nickgior@gmail.com>
2021-10-28 13:26:27 -0400

    Completed Day 1 of 100 for 100 days of Swift. Went over Variables, Constants, Integers, Strings, Doubles, and Booleans.

---
+++ Day 1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+// DAY 1 of 100 days of Swift
+// Variables, Strings, Integers, Doubles, Booleans, Constants
+var greeting = "Hello, playground"
+greeting = "Goodbye!"
+
+var age = 24
+var your_age = "Your age is \(age) years old!"
+
+let pi = 3.14 // this is a constant, pi can't change value
+
+var boo = true
+
+var str1 = """
+This string can go
+over
+multiple lines
+"""
+
+let year: Int = 12
+var month: String = "October"
